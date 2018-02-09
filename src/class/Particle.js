99de780641cac0e641212bd8030f4649c5f2d207
Bastien OSTALOWSKI <bastien.ostalowski@gmail.com>
2018-02-09 17:43:17 +0100

    add gravity to particle

--- src/class/Particle.js
+++ src/class/Particle.js
@@ -3,19 +3,31 @@ import { createVector as Vector } from './Vector'
 const particlePrototype = state => ({
   getX: state.position.getX,
   getY: state.position.getY,
+  setX: state.position.setX,
+  setY: state.position.setY,
 
   update: function() {
+    state.velocity.addTo(state.gravity)
     state.position.addTo(state.velocity)
     return this
-  }
+  },
+
+  accelerate: state.velocity.addTo
 })
 
-export const createParticle = (x = 0, y = 0, speed = 0, direction = 0) => {
+export const createParticle = (
+  x = 0,
+  y = 0,
+  speed = 0,
+  direction = 0,
+  gravity = 0
+) => {
   const state = {
     position: Vector(x, y),
     velocity: Vector()
       .setLength(speed)
-      .setAngle(direction)
+      .setAngle(direction),
+    gravity: Vector(0, gravity)
   }
 
   return Object.assign(state, particlePrototype(state))
--- src/index.js
+++ src/index.js
@@ -7,18 +7,75 @@ window.onload = () => {
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight)
 
-  let particle = Particle(100, 100, 3, Math.PI / 6)
+  let particle = Particle(100, 100)
+  let acceleration = Vector()
 
+  const onKeyDown = (event) => {
+    switch(event.keyCode) {
+      case 38: // up
+        acceleration.setY(-0.1)
+        break
+      case 40: // down
+        acceleration.setY(0.1)
+        break
+      case 37: // left
+        acceleration.setX(-0.1)
+        break
+      case 39: // right
+        acceleration.setX(0.1)
+        break;
+      default:
+        break
+    }
+  }
+
+  const onKeyUp = (event) => {
+    switch(event.keyCode) {
+      case 38: // up
+        acceleration.setY(0)
+        break
+      case 40: // down
+        acceleration.setY(0)
+        break
+      case 37: // left
+        acceleration.setX(0)
+        break
+      case 39: // right
+        acceleration.setX(0)
+        break;
+      default:
+        break
+    }
+  }
+
+  document.body.addEventListener('keydown', onKeyDown)
+  document.body.addEventListener('keyup', onKeyUp)
+  
   const update = () => {
     context.clearRect(0, 0, width, height)
 
+    particle.accelerate(acceleration)
     particle.update()
     context.beginPath()
     context.arc(particle.getX(), particle.getY(), 10, 0, Math.PI * 2, false)
     context.fill()
+
+    if (particle.getX() > width) {
+      particle.setX(0)
+    }
+    if (particle.getX() < 0) {
+      particle.setX(width)
+    }
+    if (particle.getY() > height) {
+      particle.setY(0)
+    }
+    if (particle.getY() < 0) {
+      particle.setY(height)
+    }
 
     requestAnimationFrame(update)
   }
 
   update()
 }
+
